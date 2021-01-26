70bb20d090695d63e1349f414ee2f940aba682e3
Celso Fabri Junior <celso.fabri@live.com>
2021-01-26 16:49:53 -0300

    feat: add structure to header component and styles

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyledWrapper} from 'assets/global/styled';
+import {StyledHeader, StyledLogo} from './styled';
+import LogoPokemon from 'assets/images/pokemon-logo.png';
+
+const Header = () => {
+
+  return(
+    <StyledHeader>
+      <StyledWrapper>
+      <StyledLogo>
+        <img src={LogoPokemon} width="150" height="auto" alt="Pokémon"/>
+        </StyledLogo>
+      </StyledWrapper>
+    </StyledHeader>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import {StyledWrapper} from 'assets/global/styled';
+
+export const StyledHeader = styled.header`
+  display: block;
+  width: 100%;
+`;
+
+export const StyledLogo = styled.h1`
+  display: block;
+  max-width: 300px;
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`
