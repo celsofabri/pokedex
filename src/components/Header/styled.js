2ab14be872a170e3956e28476fde9a54fe714af7
Celso Fabri Junior <celso.fabri@live.com>
2021-02-03 12:41:28 -0300

    fix: remove header position fixed

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -3,10 +3,6 @@ import { StyledWrapper } from 'assets/global/styled';
 import { colors } from 'assets/global/tokens';
 
 export const StyledHeader = styled.header`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 99;
   display: block;
   width: 100%;
   background-color: ${colors.gray100};
